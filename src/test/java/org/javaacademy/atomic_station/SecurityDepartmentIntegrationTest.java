dd782ae8641d8e080969b48b461a69c1a128fbd1
Alexandwere <[lazukov.ao@gmail.com]>
2024-11-26 20:09:38 +0300

    chore

--- src/test/java/org/javaacademy/atomic_station/SecurityDepartmentIntegrationTest.java
+++ src/test/java/org/javaacademy/atomic_station/SecurityDepartmentIntegrationTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("france")
 @DisplayName("Отдел безопасности - интеграционный")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class SecurityDepartmentIntegrationTest {
     private static final int EXPECTED_ACCIDENTS = 1;
 
